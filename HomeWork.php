fb7cfca0c3748239e48d8703a66acd1588fb3897
DozaVitamin <balex-2015@mail.ru>
2016-10-14 14:50:23 +0300

    HomeWork

---
+++ HomeWork.php
@@ -0,0 +1,94 @@
+<?php
+   class Heap{
+
+    private $heap = Array();
+
+    function __construct(...$elements)
+    {
+        if ($elements != null){
+            foreach ($elements as $element){
+                $this->push($element);
+            }
+        }
+    }
+
+
+    public function push(...$elements){
+        //use this function to add a new element
+        foreach ($elements as $element){
+            $this->heap[] = $element;
+            $this->sort();
+        }
+
+    }
+
+    public function pop(){
+        //use this function to delete the last (the biggest) element of the heap
+        if(!$this->isEmpty()){
+            unset($this->heap[count($this->heap) - 1]);
+        } else{
+            echo("No elements to delete.");
+        }
+    }
+
+    public function  peek(){
+        //use this function to get the last element of the heap
+        if(!$this->isEmpty()){
+            return($this->heap[count($this->heap) - 1]);
+        } else{
+            echo("No elements to show.");
+            return null;
+        }
+    }
+
+    public function isEmpty(){
+        //use this function to check if heap is empty
+        return empty($this->heap);
+    }
+
+    private function sort(){ //I wasn't sure whether I was allowed to use integrated PHP functions of sorting,
+        $switch = true;     //  that's why I wrote my own function, based on bubble sorting algorithm
+        while ($switch){
+            $switch = false;
+            for($i = 0; $i < count($this->heap) - 1; $i++){
+                if($this->heap[$i] > $this->heap[$i+1]){
+                    $temp = $this->heap[$i+1];
+                    $this->heap[$i+1] = $this->heap[$i];
+                    $this->heap[$i] = $temp;
+                    $switch = true;
+                }
+            }
+        }
+    }
+
+
+    public function printAll(){
+        //This function is just for convenient checking how my class works. Not for use.
+        print("</br> Checking function </br>");
+        foreach ($this->heap as $a){
+            print($a."</br>");
+        }
+        var_dump($this->heap);
+        print("</br></br>");
+
+    }
+
+   }
+
+    //these are some values for fast checking how my class works.
+    //$myHeap = new Heap(); //uncomment this to check how it works with 0 objects in array
+    $myHeap = new Heap("mdaaa",1,21,7,3,4,0,2,6,9,5,41, "lol");//comment this to check how it works with 0 objects in array
+    $myHeap->printAll();
+    var_dump($myHeap->peek());
+    $myHeap->pop();
+    $myHeap->printAll();
+    $myHeap->push(11,3);
+    $myHeap->printAll();
+
+
+    //by the way - are we gonna get any feedback about our homework? because like it's kinda important
+    // to know whether you are doing everything right or to know what you need to improve
+
+
+
+?>
